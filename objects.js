c1df1599e4a6198802280469b18fa056c888c8a6
Jason Welsh <jasonbwelsh@gmail.com>
2018-06-04 17:22:52 -0400

    created Customer sublass and extended Person

--- objects.js
+++ objects.js
@@ -16,8 +16,29 @@ class Person {
     const ageDate = new Date(diff);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
+
+  changesName(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
+  }
 }
 
 const jason = new Person('Jason', 'Welsh', '08-25-1980');
 
-console.log(jason.calculateAge());
+// Customer subclass
+
+class Customer extends Person {
+  constructor(firstName, lastName, dob, phone, membership) {
+    super(firstName, lastName, dob);
+
+    this.phone = phone;
+    this.membership = membership;
+  }
+}
+
+const zav = new Customer('Zav', 'Xargo', '04-21-1975','555-5555', 'Standard');
+console.log(zav);
